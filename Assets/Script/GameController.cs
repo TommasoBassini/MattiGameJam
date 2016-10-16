b90848343061653b930880ec083fb1234ace7e70
Tommaso Bassini <tommaso.bassini@gmail.com>
2016-10-16 21:56:24 +0200

    TOMMY IL PIU' FIO DEL REAME

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            
+        }
+    }
+}
